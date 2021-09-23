2fb20494aed03771f3d52efe741c7cbacf64df58
Evan Derickson <3587185+dericke@users.noreply.github.com>
2021-09-22 19:48:28 -0700

    Add networkx and local copy of DB

--- movie_query/main.py
+++ movie_query/main.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
-from typing import Generator, Iterable, List, Set
+import json
+import sys
+from typing import Generator, List, Optional, Set
 
+import networkx as nx
 import pandas as pd
 import requests
 from cachecontrol import CacheControl
+from requests.models import HTTPError
+from requests.sessions import HTTPAdapter
 
 
 class MovieDatabaseServer:
@@ -17,8 +22,24 @@ class MovieDatabaseServer:
 
     def __init__(self, size="small") -> None:
         self.size = size
+        self.graph = nx.Graph()
 
-    def get_database(self, caching=True) -> dict:
+        try:
+            with open(f"data/{self.sizes[self.size]}.json") as f:
+                self.raw_database = json.load(f)
+        except FileNotFoundError:
+            try:
+                self.raw_database = self.download_database()
+            except (HTTPError, ConnectionError):
+                print(
+                    "Could not obtain a suitable database either locally or online, exiting."
+                )
+                sys.exit(1)
+
+    def get_database(self):
+        return self.raw_database
+
+    def download_database(self, caching=True) -> dict:
         base_sess = requests.session()
         cached_sess = CacheControl(base_sess)
         session = cached_sess if caching else base_sess
@@ -91,25 +112,73 @@ class MovieDatabaseServer:
             costars |= movie_stars
         return costars
 
-    def get_degrees_of_separation(self, actor_id1, actor_id2) -> int:
-        pass
+    def build_actor_connection_graph(self) -> nx.Graph:
+        indexed_movies = self.movies_relational_dataframe[["actor", "index"]]
+        indexed_movies.rename(columns={"index": "movie"}, inplace=True)
+
+        # data_to_merge = indexed_movies.drop_duplicates()
+        indexed_movies = indexed_movies.merge(
+            indexed_movies.rename(columns={"actor": "actor_2"}), on="movie"
+        )
+        indexed_movies = indexed_movies[
+            ~(indexed_movies["actor"] == indexed_movies["actor_2"])
+        ].dropna()[["actor", "actor_2", "movie"]]
+
+        graph = nx.from_pandas_edgelist(
+            indexed_movies, source="actor", target="actor_2", edge_attr="movie"
+        )
+        graph.add_nodes_from(nodes_for_adding=list(self.actors_dataframe["index"]))
+        return graph
+
+    def get_degrees_of_separation(
+        self, actor1_id: str, actor2_id: str
+    ) -> Optional[int]:
+        path = nx.shortest_path(
+            self.build_actor_connection_graph(), actor1_id, actor2_id
+        )
+        if not path:
+            # No connection
+            return None
+        return len(path) - 1
 
 
-database = MovieDatabaseServer()
+def get_kbn(database: MovieDatabaseServer, other_actor_name) -> int:
+    actor2_id = database.get_actor_id_by_name(other_actor_name)
+    return database.get_degrees_of_separation()
 
-kb_id = database.get_actor_id_by_name("Kevin Bacon")
+
+def get_kb_id(database: MovieDatabaseServer) -> str:
+    return database.get_actor_id_by_name("Kevin Bacon")
+
+
+def get_kb_movie_title(database: MovieDatabaseServer) -> str:
+    kb_movie_id = next(database.get_actors_movie_ids(kb_id))
+    return database.get_movie_title_by_id(kb_movie_id)
+
+
+def get_kb_costars(database: MovieDatabaseServer) -> List[str]:
+    kb_costar_ids = database.get_costar_ids(kb_id)
+    return [database.get_actor_name_by_id(actor_id) for actor_id in kb_costar_ids]
+
+
+database = MovieDatabaseServer("small")
+
+
+kb_id = get_kb_id(database)
 print(kb_id)
 
 
-kb_movie_id = next(database.get_actors_movie_ids(kb_id))
-kb_movie_title = database.get_movie_title_by_id(kb_movie_id)
+kb_movie_title = get_kb_movie_title(database)
 print(f"Kevin Bacon has starred in {kb_movie_title}.")
 
 
-kb_costar_ids = database.get_costar_ids(kb_id)
-kb_costar_names = [
-    database.get_actor_name_by_id(actor_id) for actor_id in kb_costar_ids
-]
 print("Kevin Bacon has co-starred with these actors:")
-for star in kb_costar_names:
+for star in get_kb_costars(database):
     print(f"    {star}")
+
+other_actor = "Tom Hanks"
+other_actor_id = database.get_actor_id_by_name(other_actor)
+
+kbn = database.get_degrees_of_separation(kb_id, other_actor_id)
+
+print(f"{other_actor} has {kbn} degrees of separation from Kevin Bacon")
